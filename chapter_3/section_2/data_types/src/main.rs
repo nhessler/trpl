1c77158fcd5200e62a48f27d03e35ad9381207d2
Nathan Hessler <xmilestegx@gmail.com>
2024-01-31 14:02:52 -0600

    boolean type examples

--- chapter_3/section_2/data_types/src/main.rs
+++ chapter_3/section_2/data_types/src/main.rs
@@ -1,17 +1,5 @@
 fn main() {
-    // addition
-    let sum = 5 + 10;
+    let t = true;
 
-    // substraction
-    let difference = 95.5 - 4.3;
-
-    // multiplication
-    let product = 4 * 30;
-
-    // division
-    let quotient = 56.7 / 32.2;
-    let truncated = 5 / 3; // Results in -1
-
-    // remainder
-    let remainder = 43 % 5;
+    let f: bool = false; // explicit type annotation
 }

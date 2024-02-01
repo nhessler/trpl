1de1230339c77506a618b0c3ca81774241cc1b79
Nathan Hessler <xmilestegx@gmail.com>
2024-02-01 14:43:57 -0600

    if conditionals must return a bool type

--- chapter_3/section_5/control_flow/src/main.rs
+++ chapter_3/section_5/control_flow/src/main.rs
@@ -1,9 +1,7 @@
 fn main() {
-    let number = 7;
+    let number = 3;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    if number {
+        println!("the number was 3");
     }
 }

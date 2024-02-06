748fd57e69b8b38d1d8332588b6cdb1e46aba158
Nathan Hessler <xmilestegx@gmail.com>
2024-02-06 17:02:56 -0600

    using while for conditional looping

--- chapter_3/section_5/loops/src/main.rs
+++ chapter_3/section_5/loops/src/main.rs
@@ -1,25 +1,11 @@
 fn main() {
-    let mut count = 0;
+    let mut number = 3;
 
-    'counting_up: loop {
-        println!(" count = {count}");
-        let mut remaining = 10;
+    while number != 0 {
+        println!("{number}!");
 
-        loop {
-            println!("remaining = {remaining}");
-
-            if remaining == 9 {
-                break;
-            }
-
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-
-        count += 1;
+        number -= 1;
     }
 
-    println!("Ending count = {count}");
+    println!("LIFTOFF!!!");
 }
